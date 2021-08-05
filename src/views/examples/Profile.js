f8fc325ee9a865352ac26f5f2697b917ccdc10c6
moradalkhatib <muradalkhateb@icloud.com>
2021-08-05 15:41:09 +0300

    start with my profile

--- src/views/examples/Profile.js
+++ src/views/examples/Profile.js
@@ -23,7 +23,7 @@ import { Button, Card, Container, Row, Col } from "reactstrap";
 // core components
 import DemoNavbar from "components/Navbars/DemoNavbar.js";
 import SimpleFooter from "components/Footers/SimpleFooter.js";
-import profile from "assets/img/theme/team-4-800x800.jpg";
+import profile from "assets/img/theme/New.jpg";
 
 class Profile extends React.Component {
   componentDidMount() {
@@ -108,47 +108,46 @@ class Profile extends React.Component {
                     <Col className="order-lg-1" lg="4">
                       <div className="card-profile-stats d-flex justify-content-center">
                         <div>
-                          <span className="heading">22</span>
-                          <span className="description">Friends</span>
+                          <span className="heading">3</span>
+                          <span className="description">Programming Languages</span>
                         </div>
                         <div>
-                          <span className="heading">10</span>
+                          <span className="heading">3</span>
                           <span className="description">Photos</span>
                         </div>
                         <div>
-                          <span className="heading">89</span>
-                          <span className="description">Comments</span>
+                          <span className="heading">2</span>
+                          <span className="description">certify</span>
                         </div>
                       </div>
                     </Col>
                   </Row>
                   <div className="text-center mt-5">
                     <h3>
-                      Jessica Jones{" "}
-                      <span className="font-weight-light">, 27</span>
+                      Murad Alkhatib{" "}
+                      <span className="font-weight-light">, 24</span>
                     </h3>
                     <div className="h6 font-weight-300">
                       <i className="ni location_pin mr-2" />
-                      Bucharest, Romania
+                      Amman, Jordan
                     </div>
                     <div className="h6 mt-4">
                       <i className="ni business_briefcase-24 mr-2" />
-                      Solution Manager - Creative Tim Officer
+                      Full Stack Developer - Creative Abdul Aziz Al Ghurair School of Advanced Computing (ASAC)
                     </div>
                     <div>
                       <i className="ni education_hat mr-2" />
-                      University of Computer Science
+                      University of Computer Information System
                     </div>
                   </div>
                   <div className="mt-5 py-5 border-top text-center">
                     <Row className="justify-content-center">
                       <Col lg="9">
                         <p>
-                          An artist of considerable range, Ryan — the name taken
-                          by Melbourne-raised, Brooklyn-based Nick Murphy —
-                          writes, performs and records all of his own music,
-                          giving it a warm, intimate feel with a solid groove
-                          structure. An artist of considerable range.
+                        I love to work and learn everything new and important especially what
+                        related to my field of work as a programmer in terms of designs, codes, programs,
+                        and I hope to learning make games I aspire to be an important person in this
+                        life, I love to help others, I can work with a team and I hope the best for everyone.
                         </p>
                         <a href="#pablo" onClick={e => e.preventDefault()}>
                           Show more
